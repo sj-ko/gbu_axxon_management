3d94a25d7879a8d0635ec4b65d61a65e4d62355f
KIMHYEHIN <fhteh1@gbudatalinks.com>
2017-08-16 18:28:55 +0900

    add

--- Device_List_0.01/Camera.cs
+++ Device_List_0.01/Camera.cs
@@ -4,9 +4,17 @@ using System.Xml.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net;
+using System;
+using System.Text;
+using System.Linq;
 
 namespace Device_List_0._01
 {
+    public class json_camera
+    {
+        public string FriendlyNameLong { get; set; }
+        public string State { get; set; }
+    }
     public class management
     {
         public void serialize(Xmlclass x)
@@ -16,32 +24,53 @@ namespace Device_List_0._01
             xs.Serialize(fs, x);
             fs.Close();
         }
-
-        public void jasondata()
+        //string json = this.Request_Json();
+        //this.ParseJson(json);
+        private string Request_Json()       //Refresh 버튼을 누르면 http로 json을 가져온다
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://192.168.0.109/video-origins/DESKTOP-U8VKICK");
-            httpWebRequest.Credentials=new NetworkCredential("user", "password");
-            //httpWebRequest.PreAuthenticate = true;
-            var response = httpWebRequest.GetResponse();
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
+            string result = null;
+            string url = "http://192.168.0.109/video-origins/DESKTOP-U8VKICK";
+            Console.WriteLine("url : " + url);
 
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            StringBuilder postParams = new StringBuilder();
+
+
+            byte[] byteDataParams = UTF8Encoding.UTF8.GetBytes(postParams.ToString());
+
+            try
+            {       //HttpWebRequest -> for web parshing? 
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);            //요청 만들기
+
+                request.Credentials = new NetworkCredential("root", "root");
+
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();          //요청 받기..?
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                result = reader.ReadToEnd();                                        //현재 위치부터 스트림의 끝까지 모든 문자 읽기
+                stream.Close();
+                response.Close();
+            }
+            catch (Exception e)
             {
-                string json = "{\"user\":\"root\"," +
-                              "\"password\":\"root\"}";
-
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
+                Console.WriteLine(e.Message);
             }
 
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-            }
+            return result;
+        }
+        private void ParseJson(String json)             //JSON Parse 받아온 json 문자열 파싱
+        {
+            List<json_camera> issues = new List<json_camera>();     //issues 리스트 생성
 
+            JObject obj = JObject.Parse(json);          //JObject 클래스, JSON 문자열 구하기
+            List<JToken> array = obj.Children().ToList();//JArray 클래스
+            foreach (JToken itemObj in array)
+            {
+                json_camera issue = new json_camera();                      //리스트에 Parse한 문자열 집어넣기
+                issue.FriendlyNameLong = itemObj.First.SelectToken("friendlyNameLong").ToString();
+                issue.State = itemObj.First.SelectToken("state").ToString();
+                issues.Add(issue);              //리스트 추가
+            }
         }
     }
     public class Xmlclass
